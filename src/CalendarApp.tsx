acbf71a1ad4d744a0cc7a86ad52de48b781c3cc0
Ander Frank Carrasco <anderfrankdev@gmail.com>
2023-10-01 15:55:17 -0400

    first commit - auth page html and css ready

---
+++ src/CalendarApp.tsx
@@ -0,0 +1,13 @@
+import { AppRouter } from './router/AppRouter';
+import './App.css'
+
+function CalendarApp() {
+
+  return (
+    <>
+      <AppRouter/>
+    </>
+  )
+}
+
+export default CalendarApp
---
+++ src/auth/pages/LoginPage.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { LoginForm } from "../components/LoginForm"
+import { RegisterForm } from "../components/RegisterForm"
+
+
+export const LoginPage = ()=>{
+
+    const [action,setAction] = useState("login")
+    
+    const actions:any = {
+        login: <LoginForm setAction={setAction}/>,
+        register: <RegisterForm setAction={setAction}/>
+    }
+    
+    return (
+        <section className="bg-gray-50  dark:bg-gray-900 dark:bg-gradient-to-bl  dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 bg-gradient-to-bl from-rose-100 to-teal-100">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a
+                href="#"
+                className="flex items-center text-black mb-6 text-2xl font-bold  dark:text-white"
+                >
+                <img
+                    className="w-14 h-14 mr-4 "
+                    src="/calendar.svg"
+                    alt="logo"
+                />
+                MERN Calendar
+                </a>
+
+                {actions[action]}     
+
+            </div>
+        </section>
+    )
+}
